0bd0206b13645083de59461ea5ca0fef96b7cff3
Ernesto Anaya <ernestex@gmail.com>
2023-08-14 16:36:52 +0000

    Navegación hasta página editar musico

---
+++ src/main/js/pages/editar-musico.js
@@ -0,0 +1,17 @@
+const React = require('react');
+const {useParams, Link} = require('react-router-dom');
+
+const PageEditarMusico = ()=>{
+
+    const {id} = useParams();
+
+    return(
+        <>
+            <h1>Editar Musico: {id}</h1>
+            <Link to="/">Volver</Link>
+        </>
+    )
+
+}
+
+module.exports = PageEditarMusico
